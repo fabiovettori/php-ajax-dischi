56f15efa1a64748f82c9ba33df55415ce490cc7d
Fabio Vettori <fabio_vettori@hotmail.it>
2020-12-17 19:27:47 +0100

    ajax ok

--- ajax/src/main.js
+++ ajax/src/main.js
@@ -1,24 +1,44 @@
-var $ = require( "jquery" );
+const $ = require( "jquery" );
+const Handlebars = require("handlebars");
 
 $(document).ready(function(){
 
-    // console.log('test');
-
-    var source = $("entry-template").html();
+    var source = $("#entry-template").html();
     var template = Handlebars.compile(source);
 
     $.ajax({
-        url: '..dischi.php',
+        url: 'dischi.php',
         method: 'GET',
         success: function(response){
-            console.log(response);
+
+            // estraggo i generi contenuti nella raccolta
+            var genres = [];
+            for (var i = 0; i < response.length; i++) {
+                if (!genres.includes(response[i].genre)) {
+                    genres.push(response[i].genre);
+                };
+            };
+
+            for (var i = 0; i < genres.length; i++) {
+                $('.generi').append(`<option value="${genres[i]}">${genres[i]}</option>`);
+            }
+
+            // raggruppo i risultati degli oggetti contenuti nella chiamata per poi stamparli in pagina con handlebars
+            for (var i = 0; i < response.length; i++) {
+                var context = {
+                    poster: response[i].poster,
+                    title: response[i].title,
+                    author: response[i].author,
+                    genre: response[i].genre,
+                    year: response[i].year
+                };
+
+                var html = template(context);
+                $('.card-container').append(html);
+            };
         },
         error: function(){
             console.log('source not found');
         }
     });
-
-    // var context = {title: "My New Post", body: "This is my first post!"};
-    // var html = template(context);
-
 });
