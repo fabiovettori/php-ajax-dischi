e9a7b795f439dd99bce2eccd509330d74a0c57a6
Fabio Vettori <fabio_vettori@hotmail.it>
2020-12-17 15:19:31 +0100

    init

---
+++ php/index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>php-ajax-dischi</title>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="php-ajax-dischi">
+    </head>
+    <body>
+
+    </body>
+</html>
